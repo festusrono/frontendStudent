28366f6324ae12b2515f243942d14b7242a9c01e
festusrono <festusrono56@gmail.com>
2026-06-23 10:02:59 +0300

    registering and rerouting routers to it's specific components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,19 +1,15 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { Component, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
-import { Header } from './header/header';
-import { ProjectList } from './project-list/project-list';
-import { StudentList } from './student-list/student-list';
-import { StudentForm } from './student-form/student-form';
-import { StudentProjects } from './student-projects/student-projects';
-import { Assignment } from './assignment/assignment';
+
+
 
 @NgModule({
-  declarations: [App, Header, ProjectList, StudentList, StudentForm, StudentProjects, Assignment],
+  declarations: [App],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideHttpClient(), provideBrowserGlobalErrorListeners(), provideClientHydration()],
   bootstrap: [App],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProjectList } from './project-list/project-list';
+import { StudentList } from './student-list/student-list';
+import { StudentForm } from './student-form/student-form';
+import { Assignment } from './assignment/assignment';
+
+
+const routes: Routes = [
+  {path: "students",component:StudentList},
+  {path: "students/addstudent", component: StudentForm},
+  {path: "projects", component: ProjectList},
+  {path: "assignment", component: Assignment},
+  {path: "", redirectTo: "students", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), ],
