b0c0b5500490d8ad1c4828e2961edf25fdc980df
festusrono <festusrono56@gmail.com>
2026-06-23 10:30:49 +0300

    adding header doc

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
+import { Header } from './header/header';
 
 
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, Header],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideHttpClient(), provideBrowserGlobalErrorListeners(), provideClientHydration()],
   bootstrap: [App],
